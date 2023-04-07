5ddeb70d62eac782c121798a1fc5b53c06dc15fc
DrewMcArthur <DrewMcArthur1@gmail.com>
2023-04-06 22:27:56 -0600

    update server/test and create ai-plugin.json

--- server.ts
+++ server.ts
@@ -1,17 +1,45 @@
 import { exec } from "child_process";
 import express from "express";
 
-const app = express();
-const port = 8080;
-app.post( "/folders/:folder/build", ( req, res ) =>
+const getBuildCommand = ( language: string ) =>
 {
-  const folder = req.params["folder"];
-  console.log( `got build request for ${folder}` );
-  exec(`bash ../${folder}/build.sh`, (error, stdout, stderr) => {
-    if ( error ) res.status( 500 ).send( error  );
-    else if ( stderr ) res.status( 400 ).json( { "err": stderr } );
-    else if ( stdout ) res.status( 200 ).json( { "output": stdout } );
-    console.log('build finished.')
-  })
-})
-app.listen(port, () => console.log(`Listening on port ${port}`));
+  // switch on language to give build cmd
+  let buildCmd;
+  switch ( language )
+  {
+    case "rust":
+      return `cargo build`;
+    default: throw new Error( `unknown language ${language}` );
+  }
+}
+
+const app = express();
+const port = 8081;
+
+app.get( "/.well-known/ai-plugin.json", ( req, res ) =>
+  res.sendFile( __dirname + '/ai-plugin.json' )
+)
+
+app.post( "/folders/:folder/build/:language", ( req, res ) =>
+{
+  const folder = req.params[ "folder" ];
+  const language = req.params[ "language" ]
+  const buildCommand = getBuildCommand( language );
+
+  console.log( `got build request for ${folder} in language ${language}` );
+  console.log( `running "${buildCommand}"` );
+  exec( buildCommand, { cwd: `../${folder}` },
+    ( error, stdout, stderr ) =>
+    {
+      console.log( "got response: " );
+      console.log( "stdout: " + stdout );
+      console.log( "stderr: " + stderr );
+      console.log( "error: " + error );
+
+      if ( error ) res.status( 500 ).send( error );
+      else res.status( 200 ).json( { "output": stdout.trim() + stderr.trim() } );
+
+      console.log( 'cmd finished.' )
+    } )
+} )
+app.listen( port, () => console.log( `Listening on port ${port}` ) );
--- test-client.ts
+++ test-client.ts
@@ -3,7 +3,7 @@ import { request } from "urllib";
 
 const server_url = "http://localhost:8080"; 
 const build_folder = "rust-cms-json-parser";
-const req_url = `${server_url}/folders/${build_folder}/build`
+const req_url = `${server_url}/folders/${build_folder}/build/rust`
 
 console.log(`POST ${req_url}`)
 request( req_url, {
